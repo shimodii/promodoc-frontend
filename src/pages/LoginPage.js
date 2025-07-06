43ac98802ad437ca9664206a740743e70e5192e7
shimodi <am.shahmoradi93@gmail.com>
2025-07-06 22:05:34 +0330

    username and password label added

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 
+
 function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -18,8 +19,8 @@ function LoginPage() {
         <p>Username</p>
         <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
         <p>Password</p>
-        <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        <button type="submit">Login</button>
+        <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} /><br />
+        <button type="submit" style={{ margin: 50 }}>Login</button>
       </form>
     </div>
   )
