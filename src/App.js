b575455d67bcbde9f96a4c6f896d9e3afead4ba0
shimodi <am.shahmoradi93@gmail.com>
2025-07-06 15:32:07 +0330

    router setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ import InboxPage from './pages/InboxPage';
 import ProtectedRoute from './routes/ProtectedRoute'
 
 function App() {
-  return ();
+  return (
+  <Routes>
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/dashboard" element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}>
+      <Route path="news" element={<NewsPage />} />
+      <Route path="inbox" element={<InboxPage />} />
+      <Route path="upload" element={<UploadPage />} />
+    </Route>
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+  );
 }
 
 export default App;
