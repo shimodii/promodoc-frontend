4d7169f8e9e4b39b2b5d3fe168bae0b66cf3bcf5
shimodi <am.shahmoradi93@gmail.com>
2025-07-06 15:24:27 +0330

    basic navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+
+function Navbar() {
+  const { logout, user } = useAuth()
+
+  return (
+    <nav style={{ background: '#ddd', padding: 10 }}>
+      <span>Welcome, {user?.username} | </span>
+      <Link to="/dashboard/news">News</Link> |{' '}
+      <Link to="/dashboard/inbox">Inbox</Link> |{' '}
+      <Link to="/dashboard/upload">Upload</Link> |{' '}
+      <Link to="/dashboard/tickets">Tickets</Link> |{' '}
+      <button onClick={logout}>Logout</button>
+    </nav>
+  )
+}
+
+export default Navbar
