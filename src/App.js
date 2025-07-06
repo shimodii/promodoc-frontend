209046e6bef456d8b9ffc0d11e03b5885833bf68
shimodi <am.shahmoradi93@gmail.com>
2025-07-06 16:34:08 +0330

    management page added with admin route

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import LoginPage from './pages/LoginPage';
 import DashboardLayout from './pages/DashboardLayout';
 import UploadPage from './pages/UploadPage';
 import InboxPage from './pages/InboxPage';
+import ManagementPage from './pages/ManagementPage';
 
 import ProtectedRoute from './routes/ProtectedRoute'
+import AdminRoute from './routes/AdminRoute';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
     <Route path="/dashboard" element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}>
       <Route path="inbox" element={<InboxPage />} />
       <Route path="upload" element={<UploadPage />} />
+      <Route path="management" element={
+        <AdminRoute><ManagementPage /></AdminRoute>
+      } />
     </Route>
   </Routes>
   );
