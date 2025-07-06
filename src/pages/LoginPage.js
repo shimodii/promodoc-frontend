0ff0db8132ef9fc70a0dd0e52780bde162d019b7
shimodi <am.shahmoradi93@gmail.com>
2025-07-06 15:20:46 +0330

    dead simple login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+
+function LoginPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const { login } = useAuth()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login({ username, password })
+  }
+
+  return (
+    <div style={{ padding: 50 }}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+        <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
