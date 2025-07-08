7da22312b02e91eb37fe69b0c33f5e84356859c1
shimodi <am.shahmoradi93@gmail.com>
2025-07-08 23:00:04 +0330

    jwt and authcontext updated

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -2,7 +2,9 @@ import { useAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom'
 
 function AdminRoute({ children }) {
-  const { user } = useAuth()
+  const { getUser } = useAuth()
+  const user = getUser();
+
   return user?.role === 'admin' ? children : <Navigate to="/dashboard/inbox" />
 }
 
--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,9 +1,9 @@
-import { Navigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
-function ProtectedRoute({ children }) {
-  const { user } = useAuth()
-  return user ? children : <Navigate to="/" />
-}
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
