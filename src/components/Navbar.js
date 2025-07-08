25f2ea8b11c5caf42842a31c69b2c6816dac42e2
shimodi <am.shahmoradi93@gmail.com>
2025-07-08 22:58:30 +0330

    updated with new AuthContet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
 function Navbar() {
-  const { logout, user } = useAuth()
+  const { logout, getUser } = useAuth()
+  const user = getUser(); // Call the function here
 
   return (
     <nav style={{ background: '#ddd', padding: 10 }}>
-      <span>Welcome, {user?.username} | </span>
+      <span>Welcome, {user?.email} | </span>
       <Link to="/dashboard/inbox">Inbox</Link> |{' '}
       <Link to="/dashboard/upload">Upload</Link> |{' '}
 
